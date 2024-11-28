2d414719e0c343fec7318c9d085aea006b1ba027
Gouravsen1202 <gouravsen757@gmail.com>
2024-11-28 17:22:39 +0530

    first commit

---
+++ enter size of array.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int u;
+	cout<<"enter you size of arry";
+	cin>>u;
+	int b[u];
+	for(int i=0;i<=u;i++){
+		cin>>b[i];
+	}
+	for(int i=0;i<=u;i++){
+		cout<<b[i]<<endl;
+	}
+}
+//jo bhi value denge apan usko repet print hone chahiye:
