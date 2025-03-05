0f48ce39269b32578e292943c28b09226a5f37dd
Gourav sen <gouravsen757@gmail.com>
2025-03-05 19:22:55 +0530

    Update enter size of array.cpp

--- enter size of array.cpp
+++ enter size of array.cpp
@@ -5,7 +5,7 @@ int main(){
 	cout<<"enter you size of arry";	cin>>u;
 	int b[u];	for(int i=0;i<=u;i++){		cin>>b[i];
 	}
-	for(int i=0;i<=u;i++){
+	for(int i=0;i<=u;i++){ 
 		cout<<b[i]<<endl;
 	}
 }
